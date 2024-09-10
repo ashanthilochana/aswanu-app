7027c9cc0c94c757adebe596b9b5ddbefe297e5b
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-09-11 00:33:03 +0530

    baseurl

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
+import { baseUrl } from './constants.js';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:5500';
+const URL = process.env.NODE_ENV === 'production' ? undefined : baseUrl;
 
 export const socket = io(URL);
